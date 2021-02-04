315c87799df7275b4543a162776839255e6cc893
antounes <matthieu.brito.antunes@gmail.com>
2021-02-04 12:34:32 +0100

    updated work

--- my-first-project/hello.py
+++ my-first-project/hello.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3.7
 
 # This is a full line comment
+# Comment each line individually if you want a block comment
 
 print("Hello, World !") # this is a trailing comment
+
+"""
+This is not a block comment
+This is a multiline string.
+"""
