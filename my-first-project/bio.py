78dd3d42f1903099126246701b79073f1897be72
antounes <matthieu.brito.antunes@gmail.com>
2021-02-04 16:04:28 +0100

    updated work

--- my-first-project/bio.py
+++ my-first-project/bio.py
@@ -1,3 +1,5 @@
 name = input("What is your name ? ")
 color = input("What is your favorite colour ? ")
 age = int(input("How old are you today ? "))
+
+print(name, "is", str(age), "years old", "and loves the color", color + ".")
