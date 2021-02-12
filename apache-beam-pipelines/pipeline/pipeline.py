63a9ce8fb569dca73dc465aca7512480d8c66be3
antounes <matthieu.brito.antunes@gmail.com>
2021-02-12 13:25:31 +0100

    started basic local pipeline for ingesting csv file and computing aggregates

--- apache-beam-pipelines/pipeline/pipeline.py
+++ apache-beam-pipelines/pipeline/pipeline.py
@@ -1,8 +1,17 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
-from options import PipeOptions
+
+class MyOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument('--input',
+                            help='Input for the pipeline',
+                            default='./data/')
+        parser.add_argument('--output',
+                            help='Output for the pipeline',
+                            default='./output/')
+
 
 options = PipelineOptions()
-p = beam.PipelineOptions(options)
-
+p = beam.Pipeline(options=options)
