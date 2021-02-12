337796e61b15b25d1aaab08637ba182582026329
antounes <matthieu.brito.antunes@gmail.com>
2021-02-12 15:25:34 +0100

    started basic local pipeline for ingesting csv file and computing aggregates

--- apache-beam-pipelines/pipeline/pipeline.py
+++ apache-beam-pipelines/pipeline/pipeline.py
@@ -13,5 +13,43 @@ class MyOptions(PipelineOptions):
                             default='./output/')
 
 
+class Split(beam.DoFn):
+    def process(self, element, *args, **kwargs):
+        Date, Open, High, Low, Close, Volume = element.split(",")
+        return[{
+            "Open": float(Open),
+            "Close": float(Close)
+        }]
+
+
+class CollectOpen(beam.DoFn):
+    def process(self, element, *args, **kwargs):
+        # Returns a list of tuples containing Date and Open value
+        result = [(1, element["Open"])]
+        return result
+
+
+# Pipeline is to be written in blocks rather than in chain
+# so that we can add future transformations
+
+# Init pipeline
 options = PipelineOptions()
 p = beam.Pipeline(options=options)
+
+# Read input csv file
+csv_lines = (p | ReadFromText(input_filename, skip_header_lines=1)
+             | beam.ParDo(Split())
+             | beam.io.WriteToText(output_filename)
+             )
+
+# The GroupByKey function allows to create a PCollection of all
+# elements for which the key (ie the left side of the tuples) is the same.
+
+mean_open = (csv_lines | beam.ParDo(CollectOpen())
+             | "Grouping keys open" >> beam.GroupByKey()
+             | "Calculating means for open" >> beam.CombineValues(
+            beam.combiners.MeanCombineFn()
+        )
+)
+
+
