5d3036f088929d6a54983f91a22cd12f6ea30cf4
antounes <matthieu.brito.antunes@gmail.com>
2021-04-17 18:58:37 +0200

    Rotate array

--- python-playground/leetcode/rotateArray.py
+++ python-playground/leetcode/rotateArray.py
@@ -1,2 +1,20 @@
 # Given an array, rotate the array to the right by k steps, k >= 0
 
+def rotate(nums, k):
+    k = k % len(nums)
+    nums[:] = nums[-k:] + nums[:-k]
+
+def rotate(nums, k):
+    i = 0
+    while i < k:
+        nums = [nums[-1]] + nums[:-1]
+        i += 1
+
+    print(nums)
+
+if __name__ == "__main__":
+    #nums = [1, 2, 3, 4, 5, 6, 7]
+    #k = 3
+    nums = [-1, -100, 3, 99]
+    k = 2
+    rotate(nums, k)
